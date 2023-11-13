580992116b0f55782a931091d8ebc2c6a3c89319
dariopellegrino00 <pelledarioamico@gmail.com>
2023-11-13 15:27:56 +0100

    Implemented a way to manage  illegal function calls in echo server

--- erlang-impl/lab03/exercise4/echo.erl
+++ erlang-impl/lab03/exercise4/echo.erl
@@ -1,22 +1,37 @@
 -module(echo).
 -export([start/0, print/1, stop/0, loop/0]).
 
-start() -> register(echo, spawn(echo, loop, [])),
-           ok.
+start() ->  try 
+                register(echo, spawn(echo, loop, []))
+            catch
+                error:badarg -> io:format("The echo server is already started~n")
+            end,
+            ok.
 
-print(Term) -> echo!{input, Term},
-               ok.
 
-stop() -> echo!{stop, "stop"},
-          ok.
+print(Term) ->  try
+                    echo!{input, Term}
+                catch 
+                    error:badarg -> io:format("The echo server has not started yet, try to run 'echo:start()'  and then rerun this command~n")
+                end,
+                ok.
+
+stop() ->   try
+                echo!{stop, "stop"}
+            catch 
+                error:badarg -> io:format("The echo server has not started yet, try to run 'echo:start()' and then rerun this command~n")
+            end,
+            ok.   
 
 loop() -> 
     receive 
         {input, M} -> io:format("~p: ~p~n", [echo, M]),
-                      loop();
-        {stop, _} -> io:format("~p stopping \n", [echo]),
-                     exit(echo),
-                     unregister(echo);
+                    loop();
+        {stop, _}  -> io:format("~p stopping \n", [echo]),
+                    exit(echo),
+                    unregister(echo);
         other -> io:format("~p: error", [echo]),
-                 loop()
+                loop()
     end.
+    
+
