02fb32b05066794e02ac1d6fe9a4101f2de844c5
Dario Pellegrino <dario.pellegrino00@gmail.com>
2024-10-30 19:37:30 +0100

    erlang expression parser v2

--- erlang-impl/expr-parser/expr.erl
+++ erlang-impl/expr-parser/expr.erl
@@ -1,18 +1,25 @@
 -module(expr).
 -export([parse/1, evaluate/1]).
 
-parse("(" ++ Expr) -> 
-	{_, Res} = parse_left(Expr), 
-	Res;
-	
-parse("~(" ++ Expr) -> 
-	{_, Res} = parse_left(Expr), 
-	{uminus, Res};
+parse(E) -> 
+	{_, Res} = parse_left(lists:filter(fun(X) -> X =/= 32 end, E)),
+	Res.
 
-parse([N|_]) when (N >= 48) and (N =< 57) -> {num, N - 48}. 
+parse_left("if" ++ E) -> 
+	{RestCond, Condition} = parse_left(E),
+	case RestCond of 
+		"then" ++ ThenExpr -> 
+			{RestElse, Then} = parse_left(ThenExpr),
+			case RestElse of 
+				"else" ++ ElseExpr -> 
+					%if there is something after the else should maube give error?
+					{Rest, Else} = parse_left(ElseExpr),
+					{Rest, {if_ , Condition, Then, Else}}
+			end
+	end;
 
 parse_left([N|Expr]) when (N >= 48) and (N =< 57) -> 
-	parse_right(Expr, {num, N-48}); % TODO in int not string!!
+	parse_right(Expr, {num, N-48}); 
 
 parse_left("(" ++ Expr) -> 
 	{[_|Rest], Left} = parse_left(Expr), 
@@ -27,7 +34,14 @@ parse_right([Op, N | Expr], Left) when (N >= 48) and (N =< 57) -> % check for op
 
 parse_right([Op, 40 | Expr], Left) -> 
 	{Rest, Right} = parse_left(Expr), 
-	{Rest, parse_op(Op, Left, Right)}.
+	{Rest, parse_op(Op, Left, Right)};
+
+% if there is a ) pop it
+parse_right([41|E], Left) -> {E ,Left};
+
+parse_right(E, Left) -> {E ,Left}.
+
+
 
 parse_op(Op, Left, Right) -> 
 	case Op of 
@@ -35,7 +49,6 @@ parse_op(Op, Left, Right) ->
 		$- -> {minus , Left, Right};
 		$* -> {times , Left, Right}
 	end.
-	
 
 evaluate(Expr) -> 
 	case Expr of 
@@ -43,7 +56,12 @@ evaluate(Expr) ->
 		{minus, Left, Right} -> evaluate(Left) - evaluate(Right);
 		{times, Left, Right} -> evaluate(Left) * evaluate(Right);
 		{uminus, Term} -> -(evaluate(Term));
-		{num, N} -> N
+		{num, N} -> N;
+		{if_, Cond, Then, Else} -> 
+			case evaluate(Cond) of
+				0 -> evaluate(Then); 
+				_ -> evaluate(Else)
+			end
 	end.
 
 
